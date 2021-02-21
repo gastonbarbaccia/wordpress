79506d9971d0255c4bb93f5834a367d304cb5768
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2021-02-21 20:27:35 -0300

    Se elimina wordfence del template

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e40a18d2bbb803f363b8728ad93336d15c2c77ec',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e40a18d2bbb803f363b8728ad93336d15c2c77ec',
+    ),
+  ),
+);
